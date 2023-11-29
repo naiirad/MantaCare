3545148ad04dec3915e42da44834aa5ba8f14d9d
Marcel <marcel_neuroth@web.de>
2023-11-29 09:47:19 +0100

    Update header labels and logo

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -14,7 +14,7 @@ type HeaderMenuLink = {
 
 export const menuLinks: HeaderMenuLink[] = [
   {
-    label: "Home",
+    label: "App",
     href: "/",
   },
   {
@@ -88,11 +88,11 @@ export const Header = () => {
         </div>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
+            <Image alt="KindKoin_Logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">KindKoin</span>
+            <span className="text-xs">Connect. Donate. Transform.</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
